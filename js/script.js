0db0321ea37994dd5435bbd176e271cef75e142a
theprogrammer141 <abdullah.k9913@gmail.com>
2026-03-29 06:50:10 +0500

    Implemented Add/Delete Cart-item & localStorage

--- js/script.js
+++ js/script.js
@@ -8,6 +8,15 @@ const cartModal = document.querySelector('#cart-modal');
 const quantityCount = document.querySelector('.quantity-count');
 const reduceQuantityButton = document.querySelector('.quantity-minus');
 const addQuantityButton = document.querySelector('.quantity-plus');
+const addToCartButton = document.querySelector('.add-to-cart');
+const cartModalDescription = document.querySelector('#cart-modal-description');
+const productTitle = document.querySelector('.product-title');
+const productPrice =
+    document.querySelector('.product-price').firstChild.textContent;
+
+let cartItem = {};
+
+const savedItem = localStorage.getItem('cartItem');
 
 thumbnails.forEach((thumbnail) => {
     thumbnail.addEventListener('click', function (e) {
@@ -67,3 +76,56 @@ addQuantityButton.addEventListener('click', function () {
     quantity += 1;
     quantityCount.textContent = quantity.toString();
 });
+
+//Add to cart functionality
+
+const addToCart = function(){
+    if(Number(quantityCount.textContent) === 0){}
+    else{
+        cartItem.title = productTitle.textContent;
+        cartItem.price = productPrice;
+        cartItem.quantity = quantityCount.textContent;
+        cartItem.totalPrice = "$" + Number(productPrice.replace('$', '')) * Number(quantityCount.textContent) + ".00";
+
+        localStorage.setItem('cartItem', JSON.stringify(cartItem));
+        renderCart(cartItem);
+        quantityCount.textContent = 0;
+    }
+}
+
+const renderCart = function(cartItem){
+    if(!Number(cartItem.quantity)){
+        cartModalDescription.textContent = 'Your cart is empty';
+    }else{
+        const html = `<div class="cart-item">
+            <img src="${productImage.src.replace('.jpg', '-thumbnail.jpg')}" alt="Product" class="cart-item-image">
+            <div class="cart-item-details">
+            <p class="cart-item-title">${cartItem.title}</p>
+            <div class="cart-item-pricing">
+            <p class="cart-item-price-quantity">${cartItem.price} x ${cartItem.quantity}</p>
+            <p class="cart-item-total-price">${cartItem.totalPrice}</p>
+            </div>
+            </div>
+            <button class="delete-item"><img src="/images/icon-delete.svg" alt="DeleteIcon"/></button>
+            </div>
+            <button class="checkout-btn">Checkout</button>
+            `;
+            cartModalDescription.innerHTML = html;
+    }   
+}
+
+if (savedItem) {
+    cartItem = JSON.parse(savedItem);
+    renderCart(cartItem);
+}
+
+addToCartButton.addEventListener('click', addToCart);
+
+cartModal.addEventListener("click", function(e){
+    if(e.target.closest('.delete-item')){
+        cartModalDescription.textContent = 'Your cart is empty';
+        localStorage.removeItem('cartItem');
+        cartItem = {};
+    }
+});
+
